bc9d4b6b9cfbfe71edde68781dae5ff4b4c85b48
someone <98348816+V01D255@users.noreply.github.com>
2022-03-17 12:12:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+game.showLongText("PLAYER 1 TEAM SELECT!", DialogLayout.Bottom)
+game.showLongText("TEAM SELECT OPTIONS:", DialogLayout.Bottom)
+game.showLongText("1: HOUNDOOM, 2: JOLTEON", DialogLayout.Full)
